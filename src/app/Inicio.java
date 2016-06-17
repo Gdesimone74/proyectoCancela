7461a3da4845aea9d27144b925d182ef0c07424b
G. De Simone <gdesimone74@gmail.com>
2016-06-17 01:37:21 -0300

    comit4

--- src/app/Inicio.java
+++ src/app/Inicio.java
@@ -21,6 +21,7 @@ public class Inicio {
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
         System.out.println("holaa");
+        System.out.println("holaa");
     }
 	    
 	private static void components() {
